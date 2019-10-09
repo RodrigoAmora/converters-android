d51d23a9cc1fd05deb7662f7b5e6db54d34cc5f8
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2019-10-09 00:55:45 -0300

    Adding log

--- app/src/main/java/br/com/rodrigoamora/converters/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/rodrigoamora/converters/ui/activity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import br.com.rodrigoamora.converters.R
@@ -16,9 +17,10 @@ import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    val TAG_LOG = "converters-android"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
         createToolbarAndActionBar()
         changeFragment(TemperatureFragment(), this, R.id.container, null)
@@ -44,26 +46,26 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // as you specify a parent activity in AndroidManifest.xml.
         when (item.itemId) {
             R.id.action_about -> {
+                Log.i(TAG_LOG, "action_about clicked")
                 changeFragment(AboutFragment(), this, R.id.container, null)
-
+                return true
             }
             else -> return super.onOptionsItemSelected(item)
         }
-
-        return true
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
             R.id.nav_distance -> {
-
+                Log.i(TAG_LOG, "nav_distance clicked")
             }
             R.id.nav_temperature -> {
+                Log.i(TAG_LOG, "nav_temperature clicked")
                 changeFragment(TemperatureFragment(), this, R.id.container, null)
             }
             R.id.nav_share -> {
-
+                Log.i(TAG_LOG, "nav_share clicked")
             }
         }
 
