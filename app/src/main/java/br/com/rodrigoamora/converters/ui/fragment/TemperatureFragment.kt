76cd61d4b11ebed50143287c2500f9dfde63deca
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2019-11-15 15:35:10 -0300

    Changing hint

--- app/src/main/java/br/com/rodrigoamora/converters/ui/fragment/TemperatureFragment.kt
+++ app/src/main/java/br/com/rodrigoamora/converters/ui/fragment/TemperatureFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import br.com.rodrigoamora.converters.R
@@ -26,6 +27,24 @@ class TemperatureFragment : Fragment() {
 
         val arrayAdapter = ArrayAdapter(activity, android.R.layout.simple_list_item_1, list)
         convert.setAdapter(arrayAdapter)
+
+        convert.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                when(position) {
+                    0 -> {
+                        temperature.hint = getString(R.string.temperture_in_celsius)
+                    }
+
+                    1 -> {
+                        temperature.hint = getString(R.string.temperture_in_fahrenheit)
+                    }
+                }
+            }
+        }
 
         bt_convert.setOnClickListener{
             convertTemperature()
