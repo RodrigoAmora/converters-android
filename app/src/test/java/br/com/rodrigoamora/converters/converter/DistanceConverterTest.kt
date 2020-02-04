a6f2c1fbf89956ff81e1827d8d3f043a78db81a1
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2020-02-04 00:29:49 -0300

    Testing distance

---
+++ app/src/test/java/br/com/rodrigoamora/converters/converter/DistanceConverterTest.kt
@@ -0,0 +1,34 @@
+package br.com.rodrigoamora.converters.converter
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+
+class DistanceConverterTest {
+
+    var distanceConverter :DistanceConverter? = null
+
+    @Before
+    fun init() {
+        distanceConverter = DistanceConverter()
+    }
+
+    @Test
+    fun kilometerToMile() {
+        val distance1 = distanceConverter?.kilometerToMile(1.0)
+        assertEquals(0.621371, distance1!!, 0.0001)
+
+        val distance2 = distanceConverter?.kilometerToMile(2.0)
+        assertEquals(1.242742, distance2!!, 0.0001)
+    }
+
+    @Test
+    fun mileToKilometer() {
+        val distance1 = distanceConverter?.mileToKilometer(1.0)
+        assertEquals(1.609344, distance1!!, 0.0001)
+
+        val distance2 = distanceConverter?.mileToKilometer(2.0)
+        assertEquals(3.218688, distance2!!, 0.0001)
+    }
+}
