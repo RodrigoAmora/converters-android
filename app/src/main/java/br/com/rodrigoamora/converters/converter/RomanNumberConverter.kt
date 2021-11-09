4fb83fc2dbee938a71d1f012fff14a293d2aa142
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2021-11-09 00:36:21 -0300

    Remaning variables

--- app/src/main/java/br/com/rodrigoamora/converters/converter/RomanNumberConverter.kt
+++ app/src/main/java/br/com/rodrigoamora/converters/converter/RomanNumberConverter.kt
@@ -2,28 +2,30 @@ package br.com.rodrigoamora.converters.converter
 
 class RomanNumberConverter {
 
-    private var numero: Int? = null
-    private val listaRomanos = arrayOf("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
-    private val listaArabicos = arrayOf(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+    private var number: Int? = null
+    private val listRomanNumbers = arrayOf("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
+    private val listDecimalNumbers = arrayOf(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
 
-    fun convertDecimalToRomanNumber(arabico: Int?): String? {
-        var arabico = arabico
-        if (arabico != null && arabico > 0) {
-            var romano = ""
+    fun convertDecimalToRomanNumber(decimal: Int?): String? {
+        var decimal = decimal
+        if (decimal != null && decimal > 0) {
+            var romanNumber = ""
 
-            for (x in listaArabicos.indices) {
-                val num = listaArabicos[x]
-                while (arabico >= num) {
-                    romano += listaRomanos[x]
-                    arabico -= num
+            for (x in listDecimalNumbers.indices) {
+                val num = listDecimalNumbers[x]
+                while (decimal >= num) {
+                    romanNumber += listRomanNumbers[x]
+                    decimal -= num
                 }
             }
-            return romano
+
+            return romanNumber
         }
+        
         return null
     }
 
     fun <E> converter(): E? {
-        return convertDecimalToRomanNumber(numero) as E?
+        return convertDecimalToRomanNumber(number) as E?
     }
 }
