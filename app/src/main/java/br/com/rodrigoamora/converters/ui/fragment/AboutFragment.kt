b7e3fa61a61f5cb3d6f70fca96e2dfea7f69babb
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-05-16 16:36:03 -0300

    Removendo imports

--- app/src/main/java/br/com/rodrigoamora/converters/ui/fragment/AboutFragment.kt
+++ app/src/main/java/br/com/rodrigoamora/converters/ui/fragment/AboutFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import br.com.rodrigoamora.converters.R
-import kotlinx.android.synthetic.main.fragment_about.*
+import kotlinx.android.synthetic.main.fragment_about.lb_version
 
 class AboutFragment : Fragment() {
 
