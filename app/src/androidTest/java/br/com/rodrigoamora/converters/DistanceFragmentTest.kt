39d148a5bba48d69713d288287f1afd6335637cf
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-12-06 18:29:33 -0300

    Implementando os testes de UI para a tela de distância

---
+++ app/src/androidTest/java/br/com/rodrigoamora/converters/DistanceFragmentTest.kt
@@ -0,0 +1,94 @@
+package br.com.rodrigoamora.converters
+
+import android.content.Context
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.DrawerActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+import br.com.rodrigoamora.converters.ui.activity.MainActivity
+import org.hamcrest.CoreMatchers
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class DistanceFragmentTest {
+
+    private lateinit var context: Context
+
+    @Before
+    fun init() {
+        ActivityScenario.launch(MainActivity::class.java)
+        this.context = InstrumentationRegistry.getInstrumentation().targetContext
+    }
+
+    @Test
+    fun testKilometerToMile() {
+        Espresso
+            .onView(ViewMatchers.withId(R.id.drawer_layout))
+            //.check(ViewMatchers(ViewAssertions.isClosed(Gravity.LEFT)))
+            .perform(DrawerActions.open())
+
+        Espresso
+            .onView(ViewMatchers.withId(R.id.nav_distance))
+            .perform(ViewActions.click())
+
+        Espresso
+            .onView(ViewMatchers.withId(R.id.input_distance))
+            .perform(ViewActions.click())
+            .perform(ViewActions.typeText("77"))
+
+        Espresso
+            .onView(ViewMatchers.withId(R.id.bt_convert))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        Espresso
+            .onView(ViewMatchers.withId(R.id.bt_convert))
+            .perform(ViewActions.click())
+
+        Espresso
+            .onView(ViewMatchers.withId(R.id.tv_result))
+            .check(ViewAssertions.matches(ViewMatchers.withText("47.85 mi")))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun testMileToKilometer() {
+        Espresso
+            .onView(ViewMatchers.withId(R.id.drawer_layout))
+            //.check(ViewMatchers(ViewAssertions.isClosed(Gravity.LEFT)))
+            .perform(DrawerActions.open())
+
+        Espresso
+            .onView(ViewMatchers.withId(R.id.nav_distance))
+            .perform(ViewActions.click())
+
+        Espresso
+            .onView(ViewMatchers.withId(R.id.input_distance))
+            .perform(ViewActions.click())
+            .perform(ViewActions.typeText("77"))
+
+        Espresso
+            .onData(CoreMatchers.anything())
+            .inAdapterView(ViewMatchers.withId(R.id.spinner_convert))
+            .atPosition(1)
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        Espresso
+            .onView(ViewMatchers.withId(R.id.bt_convert))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        Espresso
+            .onView(ViewMatchers.withId(R.id.bt_convert))
+            .perform(ViewActions.click())
+
+        Espresso
+            .onView(ViewMatchers.withId(R.id.tv_result))
+            .check(ViewAssertions.matches(ViewMatchers.withText("123.92 km")))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+}
