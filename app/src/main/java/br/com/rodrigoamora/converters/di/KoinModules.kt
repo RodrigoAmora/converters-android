f4c07eb1131d6c1aba8a512c1c56fbda787b517e
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-12-05 23:17:31 -0300

    Adicionando o Koin

---
+++ app/src/main/java/br/com/rodrigoamora/converters/di/KoinModules.kt
@@ -0,0 +1,20 @@
+package br.com.rodrigoamora.converters.di
+
+import br.com.rodrigoamora.converters.ui.viewmodel.TemperatureViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.GlobalContext
+import org.koin.dsl.module
+
+fun injectFeature() = loadFeature
+
+private val loadFeature by lazy {
+    GlobalContext.loadKoinModules(
+        listOf(
+            viewModelModule
+        )
+    )
+}
+
+val viewModelModule = module {
+    viewModel { TemperatureViewModel() }
+}
