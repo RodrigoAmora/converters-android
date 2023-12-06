f2efa01e90250af69ff18c6adeaf43774bca3f16
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-12-06 18:05:38 -0300

    Renomeando a classe de teste

--- app/src/androidTest/java/br/com/rodrigoamora/converters/TemperatureFragmentTest.kt
+++ app/src/androidTest/java/br/com/rodrigoamora/converters/TemperatureFragmentTest.kt
@@ -15,7 +15,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class TemperatureDistanceFragmentTest {
+class TemperatureFragmentTest {
 
     private lateinit var context: Context
 
