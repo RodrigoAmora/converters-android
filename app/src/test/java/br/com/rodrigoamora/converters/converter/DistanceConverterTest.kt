2faf9b30133928eb83a32a4d04c8c2d0a09c559a
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-12-05 22:57:14 -0300

    Colocando a variável distanceConverter como lazy

--- app/src/test/java/br/com/rodrigoamora/converters/converter/DistanceConverterTest.kt
+++ app/src/test/java/br/com/rodrigoamora/converters/converter/DistanceConverterTest.kt
@@ -1,34 +1,33 @@
 package br.com.rodrigoamora.converters.converter
 
-import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Test
 
 class DistanceConverterTest {
 
-    var distanceConverter :DistanceConverter? = null
+    private val distanceConverter by lazy {
+        DistanceConverter()
+    }
 
     @Before
-    fun init() {
-        distanceConverter = DistanceConverter()
-    }
+    fun init() {}
 
     @Test
     fun testConvertKilometerToMile() {
-        val distance1 = distanceConverter?.kilometerToMile(1.0)
-        val distance2 = distanceConverter?.kilometerToMile(2.0)
+        val distance1 = this.distanceConverter.kilometerToMile(1.0)
+        val distance2 = this.distanceConverter.kilometerToMile(2.0)
 
-        assertEquals(0.621371, distance1!!, 0.0001)
-        assertEquals(1.242742, distance2!!, 0.0001)
+        assertEquals(0.621371, distance1, 0.0001)
+        assertEquals(1.242742, distance2, 0.0001)
     }
 
     @Test
     fun testConvertMileToKilometer() {
-        val distance1 = distanceConverter?.mileToKilometer(1.0)
-        val distance2 = distanceConverter?.mileToKilometer(2.0)
+        val distance1 = this.distanceConverter.mileToKilometer(1.0)
+        val distance2 = this.distanceConverter.mileToKilometer(2.0)
 
-        assertEquals(1.609344, distance1!!, 0.0001)
-        assertEquals(3.218688, distance2!!, 0.0001)
+        assertEquals(1.609344, distance1, 0.0001)
+        assertEquals(3.218688, distance2, 0.0001)
     }
 }
