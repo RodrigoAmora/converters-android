7764ec1e1b3f0ab6520999fbbc3d044586954302
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-12-05 22:59:13 -0300

    Organizando os imports

--- app/src/androidTest/java/br/com/rodrigoamora/converters/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/br/com/rodrigoamora/converters/ExampleInstrumentedTest.kt
@@ -1,12 +1,9 @@
 package br.com.rodrigoamora.converters
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
-
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,7 +15,6 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("br.com.rodrigoamora.converters", appContext.packageName)
+
     }
 }
